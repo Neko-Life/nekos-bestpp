43784dce56bd7d32b4c4d250acd5764e9c18b476
Neko-Life <nekolife123579@gmail.com>
2022-11-27 00:37:42 +0700

    more feature plan and doc

--- include/nekos-best++.hpp
+++ include/nekos-best++.hpp
@@ -43,13 +43,21 @@ namespace nekos_best {
 	 * @brief Response container
 	 */
 	struct Response {
-		// response status
+		std::string protocol;
+		
+		// response status, you can use this to determine whether a request succeeded
 		long status_code;
-		// response headers
+		
+		// response headers, a key value pair
 		std::map<std::string, std::string> headers;
-		// raw json response
+
+		// raw json response, can contain error messages,
+		// probably want to add a wrapper for that but no doc to be found.
+		// It will be as it is for now.
 		nlohmann::json raw_json;
 	};
+
+	// !TODO: error struct, get_error func
 
 	/**
 	 * @brief Endpoint specification struct
@@ -67,7 +75,7 @@ namespace nekos_best {
 	std::map<std::string, EndpointSpec> get_available_endpoints();
 
 	/**
-	 * @brief Metadata struct, for gif format usually anime_name and url are populated,
+	 * @brief Metadata struct, for gif format usually only anime_name and url are populated,
 	 * 	whereas anime_name isn't populated if the image were a png format
 	 */
 	struct Meta {
@@ -86,27 +94,37 @@ namespace nekos_best {
 	};
 
 	/**
-	 * @brief Fetch to a category endpoint
+	 * @brief Get last API response, emptying the cache.
+	 * 	You should copy it somewhere else if you were gonna keep it.
 	 */
-	QueryResult fetch(const std::string& category, const int amount = 1);
+	Response get_last_request_response();
+
+	/**
+	 * @brief Fetch to a category endpoint
+	 * @param category will pick one randomly if omitted
+	 * @param amount default to 1
+	 */
+	QueryResult fetch(const std::string& category = "", const int amount = 1);
 
 	/**
 	 * @brief Fetch single image directly
-	 * @param category
-	 * @param filename image filename, variable digit of number as string
-	 * @param format generally `png` or `gif`
+	 * @param category will pick one randomly if omitted
+	 * @param filename image filename, variable digit of number as string, will pick one randomly if omitted
+	 * @param format will pick one randomly if omitted
 	 */
-	Meta fetch_single(const std::string& category, const std::string& filename, const std::string& format);
+	Meta fetch_single(const std::string& category = "", const std::string& filename = "", const image_format format = if_none);
 
 	/**
 	 * @brief Search for image
 	 * @param query search query
-	 * @param type 1 for static image format, 2 for gif image format.
-	 * 		Depending on category, it usually only accept one type.
-	 * @param category image category
-	 * @param amount result amount
+	 * @param type will pick one randomly if omitted
+	 * @param category image category, will pick randomly if omitted
+	 * @param amount result amount, default to 1
 	 */
-	QueryResult search(const std::string& query, const int type, const std::string& category = "", const int amount = 1);
+	QueryResult search(const std::string& query,
+			const image_format format = if_none,
+			const std::string& category = "",
+			const int amount = 1);
 } // nekos_best
 
 #endif // NEKOS_BEST_H
